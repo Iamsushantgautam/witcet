e98447b94a1f07e7cd3c9aa8a8ade6eb8422f728
Iamsushantgautam <159980818+Iamsushantgautam@users.noreply.github.com>
2024-05-18 16:13:28 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Witcet</title>
+    <link rel="stylesheet" href="css/chatbot.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Fonts Link For Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
+    <script src="js/chatbot.js" defer></script>
+    
+    <!-- Nav CSS -->
+    <link rel="stylesheet" href="css/nav.css" />
+    <!-- Unicons CSS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+    <script src="js/nav.js" defer></script>
+    <!--topbox CSS -->
+   <link rel="stylesheet" href="css/topbox.css" />
+      <!--blogheader CSS-->
+<link rel="stylesheet" href="css/blogheader.css" />
+      <!--blog CSS -->
+    <link rel="stylesheet" href="css/blog.css" />  
+  <link rel="stylesheet" href="css/blog.css" />
+    <!--Notes pin CSS -->
+ <link rel="stylesheet" href="css/notespin.css" />
+    
+    <!--bigbox CSS -->
+   <link rel="stylesheet" href="css/bigbox.css" />
+    
+    
+      <!----footer CSS -->
+  <link rel="stylesheet" href="css/footer.css" />
+       
+      
+      
+  </head>
+ <body>
+ <!--nav start-->
+ 
+ <nav class="nav">
+      <i class="uil uil-bars navOpenBtn"></i>
+      <a href="#" class="logo"><img src="images/navlogo.png" alt="img"></a>
+
+      <ul class="nav-links">
+    <i class="uil uil-times navCloseBtn"></i>
+  <!-- <li><a class="nav-a"href="witcet.html" >Home</a></li> -->
+ <li><a class="nav-a" href="profile.php">Profile</a></li>
+ <li><a  class="nav-a"href="p/syllabus.html"> syllabus </a></li>
+ <li><a class="nav-a"href="p/library.html"> library </a></li>
+ <li><a class="nav-a"href="p/notes.html"> Notes </a></li>
+ <li><a class="nav-a"href="p/about.html">About Us</a></li>
+ <li><a class="nav-a"href="p/contact.html">Contact Us</a></li>
+      </ul>
+
+      <i class="uil uil-search search-icon" id="searchIcon"></i>
+      <div class="search-box">
+        <i class="uil uil-search search-icon"></i>
+        <input type="text" placeholder="Search here..." />
+      </div>
+    </nav><br>
+    <!-- nav end -->
+ 
+  
+    <button class="chatbot-toggler">
+      <span class="material-symbols-rounded">mode_comment</span>
+      <span class="material-symbols-outlined">close</span>
+    </button>
+    <div class="chatbot">
+      <header>
+        <h2>chatwit</h2>
+        <span class="close-btn material-symbols-outlined">close</span>
+      </header>
+      <ul class="chatbox">
+        <li class="chat incoming">
+          <span class="material-symbols-outlined">smart_toy</span>
+          <p>Hi there 👋<br>How can I help you today?</p>
+        </li>
+      </ul>
+      <div class="chat-input">
+        <textarea placeholder="Enter a message..." spellcheck="false" required></textarea>
+        <span id="send-btn" class="material-symbols-rounded">send</span>
+      </div>
+    </div><br>
+<!--chatbot end -->
+
+   <!--top images start --> 
+<center>
+<img src="images/topimage.png" alt="img" width="350" height="300" ></center>
+
+<center><h1 style="color:black">Admit your Dream
+University through WITCET</h1></center>
+
+<center><h3>Best Education Site For Online Schooling. Get top score by learning india's largest content provider.</h3></center>
+
+
+
+<center>
+<h1 style="color:black">Our <span style="color:red"> Services</span></h1>
+<p style="font-size:20px">We Provide best services in online education</p></center>
+   <!--top image end -->
+   
+  
+   
+   
+                   <!--topbox start -->
+                   <center>
+<div class="box">
+    
+  <div class="image-box">
+    <a href="p/select the semester Btech .html">
+      <img src="images/library.png" alt="Image 1">
+    </a>
+    <a href="p/select the semester Btech.html">
+      <div class="image-caption"><h2>B.Tech Question paper</h2></div>
+  </div>   </a>  
+  
+ 
+
+   <div class="image-box">
+  <a href="p/btech 7 year question paper.html">  <img src="images/exam.png" alt="Image 2"></a>
+    <div class="image-caption"><a href="p/btech 7 year question paper.html"><h2  style="color: black;">7 year B.Tech Question paper</h2></div>
+  </div></a>
+
+<div class="image-box">
+   <a href="btech syllabus.html"> <img src="images/syllabus.png" alt="Image 2"></a>
+    <div class="image-caption"><a href="btech syllabus.html"><h2 style="color: black;">B.Tech Syallbus</h2></a></div>
+  </div>
+
+
+
+ </div>
+</center>
+
+<br><br>
+  <!--topbox end -->
+   <!--Notes pin Start -->
+   
+   <center>
+  <div class="notes">
+    <center> <h2 style="color: red;">Pin</h2> </center>
+    <div class="pin">
+   <!--  
+<marquee  behavior="scroll " direction="left"><p><span style="color: #01ffff;">&nbsp;</span><a href="https://www.witcet.online/p/soft-skills-notes.html?m=1"><span style="color:#2b00fe;">B.Tech 3rd semester </span><span style="color: #01ffff;">Computer Organization and Architecture</span><span style="color: #2b00fe;"> Handwriting notes are uploaded&nbsp;</span><span>Engineering Chemistry BAS 102 UPLOAD</span></a></p></marquee> -->
+
+<center><b><p>last 7 year B.Tech question paper uploaded</p></b></center>
+
+ <b style="color:black;font-size:20px">B.Tech Syllabus</b>
+    <ul>
+    	<li>&nbsp;<a href="https://drive.google.com/file/d/1dCwMtcKAaKinS4v7BK_4pkeAA_n-kvNM/view?usp=drivesdk"><span style="color:#000000;text-decoration:none">👉 BTech_First_Yr_Common_other
+    	_than_AG_&_BT</span></a>
+</li>
+
+    
+    
+    <li>&nbsp;<a href="https://drive.google.com/file/d/1q4FoHcu1TGVnaynd8GKmJ2gubGDxXGWn/view?usp=drivesdk"><span style="color:#000000;text-decoration:none">👉  B.Tech_2nd_Yr_CSE 
+ New</span></a>
+</li>
+    
+	<li>&nbsp;<a href="https://drive.google.com/file/d/1cUvVxktUOTBpnUBt3Wf40ws0EVfP4so0/view?usp=drivesdk"><span style="color:#000000;text-decoration:none">👉 
+ Mathematics-III_Mathematics_IV
+ _Mathematics_V</span></a>
+</li>
+	
+	<li>&nbsp;<a href="https://drive.google.com/file/d/1caztbzWN8Ntdnxd8DZWpjECHUybUoriJ/view?usp=drivesdk"><span style="color:#000000;text-decoration:none">👉  Maths IV PDE&Prob Statistics</span></a>
+</li>
+<li>&nbsp;<a href="https://drive.google.com/file/d/1hdQ4-NQl74iasVggO2wELDXhSfcAB0qr/view?usp=drivesdk"><span style="color:#000000;text-decoration:none">👉Universal Human Values and Professional Ethics (BVE301/BVE401/BVE301H/BVE401H)</span></a>
+</li>
+
+<li>&nbsp;<a href="https://drive.google.com/file/d/1hH6ySFeJ71kQj_gWT4fCAcTH8HyuJrWO/view?usp=drivesdk"><span style="color:#000000;text-decoration:none">👉Common Courses: Python Programming/ Cyber Security(BCC302/BCC301)</span></a>
+</li>
+</ul>
+  
+  
+  </div>
+  </div>
+  </center>
+  
+  <br>
+   <!--notes pin end -->
+   
+  <!--bigbox start -->
+  <hr>
+  <div class="bb"> 
+      
+  <div class="big-box">
+   <a href="p/html.html"> <img src="images/book.png" alt="Image 2"></a>
+    <div class="box-caption"><a href="p/books.html"><h2 style="color: black;">Books</h2></a>
+    </div>
+  </div>
+  
+  
+  <div class="big-box">
+   <a href="p/notes.html"> <img src="images/btechnotes.png" alt="Image 2"></a>
+    <div class="box-caption"><a href="p/notes.html"><h2 style="color: black;">B.Tech Notes</h2></a>
+    </div>
+  </div>
+  
+  <div class="big-box">
+   <a href="p/course.html"> <img src="images/syllabus.png" alt="Image 2"></a>
+    <div class="box-caption"><a href="p/course.html"><h2 style="color: black;">Course</h2></a>
+   </div>
+  </div>
+  
+   
+  
+ 
+  
+   </div>
+  <!--bigbox end -->
+  
+  
+          <!--blogheader start -->
+<section class='home-blog'>
+<h2>Our Blogs</h2>
+<a href="blogs/blogs.html">View More</a>
+<section class='clear'></section>
+</section>
+<br>
+<!--blogheader end -->
+
+<!--blog start -->
+<style>
+  
+/* Style the blog container */
+
+.blog-list {
+  padding: 20px;
+}
+
+/* Style each individual blog */
+
+.blog {
+  border: 3px solid #ccc;
+  
+  /*background-color: ;*/
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+/* Style the blog title */
+
+.blog h2 {
+  margin-bottom: 10px;
+  font-size: 1.5rem;
+  color: #333;
+}
+
+/* Style the blog content */
+
+.blog p {
+  font-size: 1rem;
+  color: #666;
+}
+</style>
+<centre>
+ <div class="blog-list">
+    <!-- Blogs -->
+    
+     <div class="blog">
+  <a href="Blogs/technology/what is an api.html">  <h2>What is an API?</h2>
+      <p>At its core, an API is a set of rules and protocols that allows different software applications to communicate with each other. 
+
+</p>
+    </div></a>
+    
+    
+    
+ <a href="Blogs\articles\what is cyber crimes.html">   <div class="blog">
+      <h2>What is Cyber Crime ? </h2>
+      <p>Computers and the internet are arguably the most influential inventions the world has ever seen. Computers multiplied the speed of humans to accomplish any task.</p>
+    </div></a>
+    
+  <a href="Blogs\articles\Define data structure.html"> <div class="blog">
+      <h2>Define data structure. Describe about its need and types.</h2>
+      <p>A data structure is a way of organizing all data items that considers not only the elements stored but also their relationship to each other.</p>
+    </div></a>
+    <!-- Add more blogs here -->
+  </div>
+ </centre>
+
+<!--blog end -->
+
+
+<center><h1>About Us</h1></center>
+<center><p>Lifelong learning is possible only for a curious learner. Each passing day is something new for us and we hope these lifelong learning quotes help you in your growth.</p></center>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70px;
+}
+
+.button {
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  background-color:#ff4069f3;
+  color:white;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.button:hover {
+  background-color: #2980b9;
+}
+</style>
+</head>
+<body>
+
+<div class="container">
+ <a href="p/about.html"><button class="button">Read More</button></a>
+</div><br><br><br>
+
+<center><h1 >WIT<span style="color:#ff4069f3">CET</span></h1></center>
+
+<center><p><a href="p/about.html"  style="text-decoration:none;color:black">About Us</a><span><a href="p/contact us. html" style="text-decoration:none;color:black">&ensp;Contact Us</a><span><a href="p/services.html" style="text-decoration:none;color:black">&ensp;Services</a></span></span></p></center>    
+    
+</center>
+
+
+
+
+
+
+ <br> <br> <br>
+<!-- footer start -->
+<br>
+    <footer class="footer">
+    <a href="p/about.html" style="color:white;"> <span> About  <span> <a href="p/contact.html" style="color:white;"> &nbsp Contact Us </span> </a></span></a>
+      <br><br>
+    &copy; 2023 Witcet. All rights reserved.
+  </footer>
+  <!-- footer end --> 
+    
+
+  </body>
+</html>
